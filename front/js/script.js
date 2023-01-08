c6377a587a2ddc79eee9f582990fa8c7792085ab
Nordine Boukahilat <nordineboukahilat@gmail.com>
2023-01-08 06:02:03 +0100

    1st element

--- front/js/script.js
+++ front/js/script.js
@@ -7,9 +7,28 @@ fetch("http://localhost:3000/api/products")
 
 
 function addProducts(donnees) {
-    const id = donnees[0]._id
-    const anchor = makeAnchor(id)
-    appendChildren(anchor)
+    const _id = donnees[0]._id
+    const imageUrl = donnees[0].imageUrl
+    const altTxt = donnees[0].altTxt
+    const name = donnees[0].name
+    const description = donnees[0].description
+
+    
+    const anchor = makeAnchor(_id)
+
+    const article = document.createElement("article")
+    const image = makeImage(imageUrl, altTxt)
+    const h3 = makeH3(name)
+    const p = makeParagraph(description)
+
+    appendElementsToArticle(article, image, h3, p)
+    appendArticleToAnchor(anchor, article)
+}
+
+function appendElementsToArticle(article, image, h3, p) {
+    article.appendChild(image)
+    article.appendChild(h3)
+    article.appendChild(p)
 }
 
 function makeAnchor(id) {
@@ -18,12 +37,33 @@ function makeAnchor(id) {
     return anchor
 }
 
-function appendChildren(anchor) {
+function appendArticleToAnchor(anchor, article) {
     const items = document.querySelector("#items")
     if (items != null) {
         items.appendChild(anchor)
+        anchor.appendChild(article)
+        console.log("elements ajouté à items", items)
 }
 }
 
+function makeImage(imageUrl, altTxt) {
+    const image = document.createElement("img")
+    image.src = imageUrl
+    image.alt = altTxt
+    image.removeAttribute("title")
+    image.removeAttribute("style")
+    return image
+}
 
-
+function makeH3(name) {
+const h3 = document.createElement("h3")
+h3.textContent = name
+h3.classList.add("productName")
+return h3
+}
+function makeParagraph(description) {
+    const p = document.createElement("p")
+    p.textContent = description
+    p.classList.add("productDescription")
+    return p
+}
