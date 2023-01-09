d70a03c1c4675c6b30106ac0bc5db955af5ef8e1
Nordine Boukahilat <nordineboukahilat@gmail.com>
2023-01-09 06:30:46 +0100

    interface + productjs

--- front/js/script.js
+++ front/js/script.js
@@ -1,17 +1,17 @@
 fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
-    .then((data) => {
-        console.log(data)
-        return addProducts(data)
-    })
+    .then((kanaps) => addProducts(kanaps))
 
 
-function addProducts(donnees) {
-    const _id = donnees[0]._id
-    const imageUrl = donnees[0].imageUrl
-    const altTxt = donnees[0].altTxt
-    const name = donnees[0].name
-    const description = donnees[0].description
+function addProducts(kanaps) {
+
+    kanaps.forEach((kanap) => {
+        
+    const _id = kanap._id
+    const imageUrl = kanap.imageUrl
+    const altTxt = kanap.altTxt
+    const name = kanap.name
+    const description = kanap.description
 
     
     const anchor = makeAnchor(_id)
@@ -21,14 +21,18 @@ function addProducts(donnees) {
     const h3 = makeH3(name)
     const p = makeParagraph(description)
 
-    appendElementsToArticle(article, image, h3, p)
+    appendElementsToArticle(article, [image, h3, p])
     appendArticleToAnchor(anchor, article)
+})
 }
 
-function appendElementsToArticle(article, image, h3, p) {
-    article.appendChild(image)
-    article.appendChild(h3)
-    article.appendChild(p)
+function appendElementsToArticle(article, array) {
+    array.forEach((item) => {
+        article.appendChild(item)
+    })
+    // article.appendChild(image)
+    // article.appendChild(h3)
+    // article.appendChild(p)
 }
 
 function makeAnchor(id) {
@@ -42,7 +46,6 @@ function appendArticleToAnchor(anchor, article) {
     if (items != null) {
         items.appendChild(anchor)
         anchor.appendChild(article)
-        console.log("elements ajouté à items", items)
 }
 }
 
